b5f5d98451cffbf8787cd9be8eda0b974a85fa6a
AshithaCDAC <ashitha@cdac.in>
2025-12-19 12:42:13 +0000

    url also changed to v2

--- Nudr_DataRepository/configuration.go
+++ Nudr_DataRepository/configuration.go
@@ -30,7 +30,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		basePath:      "https://example.com/nudr-dr/v2",
-		url:           "{apiRoot}/nudr-dr/v1",
+		url:           "{apiRoot}/nudr-dr/v2",
 		defaultHeader: make(map[string]string),
 		userAgent:     "OpenAPI-Generator/1.0.0/go",
 	}
