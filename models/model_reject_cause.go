06d9618009e348a03b902f4aed10930d4df3e3e6
bhagathkrishnacdac <bhagath.krishna@cdac.in>
2026-01-02 15:26:47 +0530

    Add RejectedSnssai and RejectCause models to support rejected network slice handling in AMF.

---
+++ models/model_reject_cause.go
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * NSSF NS Selection
+ *
+ * Models manually added to support Rejected NSSAI (based on TS 29.531/29.571)
+ *
+ */
+package models
+
+type RejectCause string
+
+const (
+	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_PLMN                 RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_PLMN"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_CURRENT_PLMN_OR_SNPN RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_CURRENT_PLMN_OR_SNPN"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_TA                   RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_TA"
+	RejectCause_PLMN_NOT_ALLOWED                              RejectCause = "PLMN_NOT_ALLOWED"
+	RejectCause_REGISTRATION_AREA_MISMATCH                    RejectCause = "REGISTRATION_AREA_MISMATCH"
+)
