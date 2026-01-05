82ca86b96efad0e05013d95a9f03d20112167aed
bhagathkrishnacdac <bhagath.krishna@cdac.in>
2026-01-05 14:43:46 +0530

    Added Reject Cause

--- models/model_reject_cause.go
+++ models/model_reject_cause.go
@@ -11,9 +11,11 @@ package models
 type RejectCause string
 
 const (
-	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_PLMN                 RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_PLMN"
-	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_CURRENT_PLMN_OR_SNPN RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_CURRENT_PLMN_OR_SNPN"
-	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_TA                   RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_TA"
-	RejectCause_PLMN_NOT_ALLOWED                              RejectCause = "PLMN_NOT_ALLOWED"
-	RejectCause_REGISTRATION_AREA_MISMATCH                    RejectCause = "REGISTRATION_AREA_MISMATCH"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_PLMN                             RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_PLMN"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_CURRENT_PLMN_OR_SNPN             RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_CURRENT_PLMN_OR_SNPN"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_IN_TA                               RejectCause = "S_NSSAI_NOT_AVAILABLE_IN_TA"
+	RejectCause_PLMN_NOT_ALLOWED                                          RejectCause = "PLMN_NOT_ALLOWED"
+	RejectCause_REGISTRATION_AREA_MISMATCH                                RejectCause = "REGISTRATION_AREA_MISMATCH"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_DUE_TO_FAILED_OR_REVOKED_NSAA       RejectCause = "S_NSSAI_NOT_AVAILABLE_DUE_TO_FAILED_OR_REVOKED_NSAA"
+	RejectCause_S_NSSAI_NOT_AVAILABLE_DUE_TO_MAXIMUM_NUMBER_OF_UE_REACHED RejectCause = "S_NSSAI_NOT_AVAILABLE_DUE_TO_MAXIMUM_NUMBER_OF_UE_REACHED"
 )
