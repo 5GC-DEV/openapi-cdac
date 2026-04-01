3d0cafcc7f49f04134ffac632ac9cf9f0dac31a3
Anaswara Nambiar <159991511+anaswarac-dac@users.noreply.github.com>
2026-04-01 11:00:51 +0530

    Update api_n1_n2_message_collection_document.go

--- Namf_Communication/api_n1_n2_message_collection_document.go
+++ Namf_Communication/api_n1_n2_message_collection_document.go
@@ -63,10 +63,10 @@ func (a *N1N2MessageCollectionDocumentApiService) N1N2MessageTransfer(ctx contex
 	)
 
 	// create path and map variables
-	// localVarPath := a.client.cfg.BasePath() + "/ue-contexts/{ueContextId}/n1-n2-messages"
-	// localVarPath = strings.Replace(localVarPath, "{"+"ueContextId"+"}", fmt.Sprintf("%v", ueContextId), -1)
+	localVarPath := a.client.cfg.BasePath() + "/ue-contexts/{ueContextId}/n1-n2-messages"
+	localVarPath = strings.Replace(localVarPath, "{"+"ueContextId"+"}", fmt.Sprintf("%v", ueContextId), -1)
 
-	basePath := a.client.cfg.BasePath()
+	/*basePath := a.client.cfg.BasePath()
 
 	// Ensure scheme exists
 	if !strings.HasPrefix(basePath, "http://") && !strings.HasPrefix(basePath, "https://") {
@@ -76,7 +76,7 @@ func (a *N1N2MessageCollectionDocumentApiService) N1N2MessageTransfer(ctx contex
 
 	// Create path
 	localVarPath := basePath + "/ue-contexts/{ueContextId}/n1-n2-messages"
-	localVarPath = strings.Replace(localVarPath, "{ueContextId}", fmt.Sprintf("%v", ueContextId), -1)
+	localVarPath = strings.Replace(localVarPath, "{ueContextId}", fmt.Sprintf("%v", ueContextId), -1)*/
 
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := url.Values{}
